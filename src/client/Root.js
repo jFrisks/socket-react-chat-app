3b3791e8484bc74dad6c4a540ea3e81b362e3154
jfrisks <jo1875fr-s@student.lu.se>
2019-06-24 23:59:54 +0200

    Started on connecteing to server a bit

--- src/client/Root.js
+++ src/client/Root.js
@@ -6,93 +6,110 @@ import MainLayout from './MainLayout';
 import ChatroomPreview from './ChatroomPreview';
 import Chatroom from './Chatroom';
 import Loader from './Loader';
+import Socket from './socket';
 
 import chatroomImage from '../public/chatrooms/alexandria.jpg';
 import userImage1 from '../public/users/carol.jpg';
 import userImage2 from '../public/users/daryl.jpg';
 import userImage3 from '../public/users/negan.jpeg';
 import userImage4 from '../public/users/rick.jpg';
+import socket from './socket';
 
+const chatroom = {
+    image: chatroomImage,
+    name: "ALEXANDRIA"
+}
 
+const messages = [
+    {
+        user: "PERSON 1",
+        message: "HEJSAN",
+        img: userImage1,
+        key: 1,
+    },
+    {
+        user: "PERSON 2",
+        message: "BLÄBLÄ",
+        img: userImage2,
+        key: 2,
+    },
+    {
+        user: "PERSON 3",
+        message: "BLÄBLÄ",
+        img: userImage3,
+        key: 3,
+    },
+    {
+        user: "PERSON 1",
+        message: "SKIT PÅ DIG",
+        img: userImage1,
+        key: 4,
+    },
+    {
+        user: "PERSON 2",
+        message: "HIHIHIH",
+        img: userImage2,
+        key: 5,
+    },
+    {
+        user: "PERSON 3",
+        message: "SNYGGT SAGT!",
+        img: userImage3,
+        key: 6,
+    },
+    {
+        user: "PERSON 3",
+        message: "BLÄBLÄ",
+        img: userImage3,
+        key: 7,
+    },
+    {
+        user: "PERSON 1",
+        message: "SKIT PÅ DIG",
+        img: userImage1,
+        key: 8,
+    },
+    {
+        user: "PERSON 2",
+        message: "HIHIHIH",
+        img: userImage2,
+        key: 9,
+    },
+    {
+        user: "PERSON 3",
+        message: "SNYGGT SAGT!",
+        img: userImage3,
+        key: 10,
+    },
+]
 
-function Root() {
+class Root extends React.Component {
 
-    const chatroom = {
-        image: chatroomImage,
-        name: "ALEXANDRIA"
+    constructor(props, context){
+        super(props);
+
+        this.state = {
+            client: socket(),
+        }
     }
-
-    const messages = [
-        {
-            user: "PERSON 1",
-            message: "HEJSAN",
-            img: userImage1,
-            key: 1,
-        },
-        {
-            user: "PERSON 2",
-            message: "BLÄBLÄ",
-            img: userImage2,
-            key: 2,
-        },
-        {
-            user: "PERSON 3",
-            message: "BLÄBLÄ",
-            img: userImage3,
-            key: 3,
-        },
-        {
-            user: "PERSON 1",
-            message: "SKIT PÅ DIG",
-            img: userImage1,
-            key: 4,
-        },
-        {
-            user: "PERSON 2",
-            message: "HIHIHIH",
-            img: userImage2,
-            key: 5,
-        },
-        {
-            user: "PERSON 3",
-            message: "SNYGGT SAGT!",
-            img: userImage3,
-            key: 6,
-        },
-        {
-            user: "PERSON 3",
-            message: "BLÄBLÄ",
-            img: userImage3,
-            key: 7,
-        },
-        {
-            user: "PERSON 1",
-            message: "SKIT PÅ DIG",
-            img: userImage1,
-            key: 8,
-        },
-        {
-            user: "PERSON 2",
-            message: "HIHIHIH",
-            img: userImage2,
-            key: 9,
-        },
-        {
-            user: "PERSON 3",
-            message: "SNYGGT SAGT!",
-            img: userImage3,
-            key: 10,
-        },
-    ]
-
-    return (
-        <MuiThemeProvider>
-            <MainLayout>
-                <Chatroom chatroom={chatroom} messages={messages}></Chatroom>
-                <ChatroomPreview></ChatroomPreview>
-            </MainLayout>
-        </MuiThemeProvider>
-    )
+    
+    render() {
+        return (
+            <MuiThemeProvider>
+                <MainLayout>
+                    <Chatroom
+                        chatroom={chatroom}
+                        messages={messages}
+                        registerHandler={this.state.client.registerHandler}
+                        unregisterHandler={this.state.client.unregisterHandler}
+                    >
+                    </Chatroom>
+                    <ChatroomPreview></ChatroomPreview>
+                </MainLayout>
+            </MuiThemeProvider>
+        )
+    }
+    
 }
 
 export default Root;
--- src/client/socket.js
+++ src/client/socket.js
@@ -1,40 +1,52 @@
 const io = require('socket.io-client')
+export default function() {
+    const socket = io.connect('http://localhost:8000')
 
-const socket = io.connect('http://localhost:8000')
+    function registerHandler(onMessageReceived) {
+        socket.on('message', onMessageReceived)
+    }
 
-function registerHandler(onMessageReceived) {
-    socket.on('message', onMessageReceived)
-}
+    function unregisterHandler() {
+        socket.off('message')
+    }
 
-function unregisterHandler() {
-    socket.off('message')
-}
+    socket.on('error', (err) => {
+        console.log('received socket error:');
+        console.log(err)
+    })
 
-socket.on('error', (err) => {
-    console.log('received socket error:');
-    console.log(err)
-})
+    function register(name, cb) {
+        socket.emit('register', name, cb)
+    }
 
-function register(name, cb) {
-    socket.emit('register', name, cb)
-}
+    function join(chatRoomName, cb) {
+        socket.emit('join', chatRoomName, cb)
+    }
 
-function join(chatRoomName, cb) {
-    socket.emit('join', chatRoomName, cb)
-}
+    function leave(chatRoomName, cb) {
+        socket.emit('leave', chatRoomName, cb)
+    }
 
-function leave(chatRoomName, cb) {
-    socket.emit('leave', chatRoomName, cb)
-}
+    function message(chatRoomName, msg, cb){
+        socket.emit('message', {chatRoomName, message: msg}, cb)
+    }
 
-function message(chatRoomName, msg, cb){
-    socket.emit('message', {chatRoomName, message: msg}, cb)
-}
+    function getChatRooms(cb) {
+        socket.emit('chatrooms', null, cb)
+    }
 
-function getChatRooms(cb) {
-    socket.emit('chatrooms', null, cb)
-}
+    function getAvailableUsers(cb) {
+        socket.emit('availableUsers', null, cb)
+    }
 
-function getAvailableUsers(cb) {
-    socket.emit('availableUsers', null, cb)
+    return {
+        register,
+        join,
+        leave,
+        message,
+        getChatRooms,
+        getAvailableUsers,
+        registerHandler,
+        unregisterHandler,
+    }
 }
