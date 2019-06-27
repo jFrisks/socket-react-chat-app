b98f58b2e684741e9da7178f26c36d46dc6cf6f6
jfrisks <jo1875fr-s@student.lu.se>
2019-06-27 23:59:23 +0200

    better dialog

--- src/client/UserSelection.js
+++ src/client/UserSelection.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
-import { Dialog, DialogTitle} from '@material-ui/core';
+import { Dialog, DialogTitle, Avatar} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
+
+import AddIcon from '@material-ui/icons/Add';
 
 const users = [
     {name: 'Carol'},
@@ -21,12 +23,21 @@ function ChooseAvatarDialog(props) {
         onClose(selectedUser)
     }
 
+    function handleListUserClick(value) {
+        onClose(value)
+    }
+
     return (
         <Dialog onClose={handleClose} {...other}>
             <DialogTitle>Choose User</DialogTitle>
             <List>
                 {users.map((user) => (
-                    <ListItem>
+                    <ListItem onClick={() => handleListUserClick(user)} key={user}>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <AddIcon/>
+                            </Avatar>
+                        </ListItemAvatar>
                         <ListItemText primary={user.name}/>
                     </ListItem>
                 ))}
