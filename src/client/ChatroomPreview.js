95f1f524667a88ec112f5ff9c29c3d04fe4bc6c5
jfrisks <jo1875fr-s@student.lu.se>
2019-07-07 23:40:31 +0200

    Abstracted more too root component, added conditional rendering for chatroom. Prepared for more advanced conditons before opening chatroom - userpick && chatroomPick

--- src/client/ChatroomPreview.js
+++ src/client/ChatroomPreview.js
@@ -5,48 +5,31 @@ import Card from '@material-ui/core/Card';
 import { CardMedia, CardContent, CardActionArea } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 
-import chatrooms1 from '../public/chatrooms/alexandria.jpg';
-import chatrooms2 from '../public/chatrooms/hilltop.jpg';
-import chatrooms3 from '../public/chatrooms/sanctuary.jpg';
-import chatrooms4 from '../public/chatrooms/terminus.jpg';
-
 const ChatroomWrapper = styled.section`
-    position: relative;
+    float: right;
     background-color: #123EDF;
     width: 70%;
     min-width: 250px;
 `
 
-let chatrooms = [
-    {title: 'Room 1', text: 'Lizards do that and that', img: chatrooms1},
-    {title: 'Room 2', text: 'Dont do it', img: chatrooms2},
-    {title: 'Room 3', text: 'Lizards do that and that', img: chatrooms3},
-    {title: 'Room 4', text: 'Lizards do that and that', img: chatrooms4},
-]
-
 function ChatroomPreview(props) {
-    const fullCards = chatrooms.map((chatroom, key) =>
-        <Card>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    image={chatroom.img}
-                    />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {chatroom.title}
-                    </Typography>
-                    <Typography component="p">
-                        {chatroom.text}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
+    const {chatroom, onEnter} = props;
 
     return (
-        <ChatroomWrapper padding>
-            {fullCards}
+        <ChatroomWrapper padding key={props.key}>
+            <Card onClick={(e) => onEnter(chatroom, e)}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        image={chatroom.image}
+                        />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {chatroom.name}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
         </ChatroomWrapper>
     )
 }
--- src/client/Home.js
+++ src/client/Home.js
@@ -12,7 +12,7 @@ export default ({
                 <ChatroomPreview 
                     key={chatroom.name}
                     chatroom={chatroom}
-                    onEnter={() => onEnterChatroom}
+                    onEnter={onEnterChatroom}
                 />
             ))
         }
