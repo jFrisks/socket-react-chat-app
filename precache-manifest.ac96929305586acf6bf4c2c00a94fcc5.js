40341ee8c4d0496b1a4de501e1bfd2580b752409
jfrisks <jo1875fr-s@student.lu.se>
2019-09-08 22:44:48 +0200

    Updates

---
+++ precache-manifest.ac96929305586acf6bf4c2c00a94fcc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31f872c6237eed2495d4142f6747ef8",
+    "url": "/socket-react-chat-app/index.html"
+  },
+  {
+    "revision": "8752308bf0f9b832480e",
+    "url": "/socket-react-chat-app/static/css/main.9ef0b74e.chunk.css"
+  },
+  {
+    "revision": "2ce5d3e86d6f238dfcf9",
+    "url": "/socket-react-chat-app/static/js/2.87270484.chunk.js"
+  },
+  {
+    "revision": "8752308bf0f9b832480e",
+    "url": "/socket-react-chat-app/static/js/main.cd352046.chunk.js"
+  },
+  {
+    "revision": "4b32747128bea86737e6",
+    "url": "/socket-react-chat-app/static/js/runtime~main.5c745f96.js"
+  },
+  {
+    "revision": "74b7f8d7436e2ec10d2d5ac0ac9dc4d2",
+    "url": "/socket-react-chat-app/static/media/stranger_things_logo.74b7f8d7.png"
+  },
+  {
+    "revision": "533a1ea7143dc0ecd5e57af1a7420260",
+    "url": "/socket-react-chat-app/static/media/strangerthings.533a1ea7.jpg"
+  }
+]);
