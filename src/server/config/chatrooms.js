f7492eb60e0049cd052dc519ef4c6a0f84c0866d
jfrisks <jo1875fr-s@student.lu.se>
2019-09-29 22:21:02 +0200

    Moved files to server-folder

---
+++ src/server/config/chatrooms.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: 'Hawkins Lab',
+    image: 'chatrooms/hawkinslab.png'
+  },
+  {
+    name: 'Upside Down',
+    image: 'chatrooms/upsidedown.jpg'
+  },
+  {
+    name: 'Starcourt',
+    image: 'chatrooms/starcourt.jpg'
+  },
+  {
+    name: 'Hawkins',
+    image: 'chatrooms/strangerthings.jpg'
+  },
+  {
+    name: 'Scoops Ahoy',
+    image: 'chatrooms/scoopsahoy.jpg'
+  }
+]
+
